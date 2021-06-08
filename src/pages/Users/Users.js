660b2be691d5c3a78293208e8dd0b21cef31ef99
amirpeace1080 <amirpeace1080@gmail.com>
2021-06-08 14:14:12 +0430

    stop: for app android

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,63 +1,54 @@
-import React from "react";
-import { Col, Container, Row, Card, Image } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import axios from "axios";
 import "./user.style.css";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.ir/users")
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="main-user">
       <h1>User Page</h1>
 
+      {
+        users.map(user => {
+          <h1>Show Json : {user.id}</h1>
+        })
+      }
+
       <Container>
         <Row>
           <Col xs={12} sm={6} lg={3}>
-            {/* start card view  */}
-            <Card className="card-main">
-              {/* <Col xs={6} md={4}> */}
-              {/* <Image
-                  src="https://bulma.io/images/placeholders/1280x960.png"
-                  className="card-image"
-                  roundedCircle
-                  height="90"
-                  width="90"
-                /> */}
-              <div className="body">
-              <div className="container">
-                <div className="card">
-                  <div className="content">
-                    <h2>01</h2>
-                    <Col xs={6} md={4}>
-                    <Image
-                  src="https://bulma.io/images/placeholders/1280x960.png"
-                  className="card-image"
-                  roundedCircle
-                  height="90"
-                  width="90"
-                /> 
-                    </Col>
-                    <h3>Card One</h3>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Eius fugiat dolores laborum harum recusandae consequuntur
-                      itaque quaerat illo rem necessitatibus unde vitae dolorem,
-                      nemo nihil fuga. Fugit corrupti assumenda consequuntur!
-                    </p>
-                    <a href="#">Read More</a>
+            <div className="body">
+              <div className="a-box">
+                <div className="img-container">
+                  <div className="img-inner">
+                    <div className="inner-skew">
+                      <img src="https://avatars.dicebear.com/api/male/mamad.svg" />
+                    </div>
+                  </div>
+                </div>
+                <div className="text-container">
+                  <h3>A blue bird</h3>
+                  <div>
+                    This a demo experiment to skew image container. It looks
+                    good.
                   </div>
                 </div>
               </div>
-              </div>
-              {/* </Col> */}
-            </Card>
-            {/* end card view */}
-          </Col>
-          <Col xs={12} sm={6} lg={3}>
-            <Card>hhh</Card>
-          </Col>
-          <Col xs={12} sm={6} lg={3}>
-            <Card>hh</Card>
-          </Col>
-          <Col xs={12} sm={6} lg={3}>
-            <Card>hhh</Card>
+            </div>
           </Col>
         </Row>
       </Container>
